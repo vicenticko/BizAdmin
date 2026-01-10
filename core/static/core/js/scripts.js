3403f9b8ea8d7800044c209496f2259515db43d5
vicenticko <vi.gilbert@duocuc.cl>
2026-01-10 18:20:10 -0300

    first commit

---
+++ core/static/core/js/scripts.js
@@ -0,0 +1,9 @@
+window.addEventListener('DOMContentLoaded', event => {
+    const sidebarToggle = document.body.querySelector('#sidebarToggle');
+    if (sidebarToggle) {
+        sidebarToggle.addEventListener('click', event => {
+            event.preventDefault();
+            document.body.classList.toggle('sb-sidenav-toggled');
+        });
+    }
+});
